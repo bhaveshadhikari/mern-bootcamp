04d7812723942f679b988fd257d014625b8479a8
redmenIshab <redmenIshab>
2026-01-12 20:44:50 +0545

    adds react router

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 // Navbar Component
 function Navbar() {
   const [isUserAuthenticated, setIsUserAuthenticated] = useState(false);
+  const navigate = useNavigate();
 
   const authenticatedUser = () => {
     setIsUserAuthenticated(true);
     console.log("User authenticated Value", isUserAuthenticated);
+    // some login or sign in logic and navigate
+    navigate("/sign-in");
   };
-  // fetch facebook Posts
-  //facebook API KEY
-  //FACEBOOKA_API_KEY -> using
 
   const renderAuthenticationScreen = () => {
     // computations
@@ -43,22 +43,15 @@ function Navbar() {
         <a href="#about" className="navbar-link">
           About Us
         </a>
-        <Link to={"/team"} classname="navbar-link">
-          Team
-        </Link>
+        <Link to={"/team"}>Team</Link>
         {/* User Clicks on Signin -> updates signin to "Get started" */}
 
         {isUserAuthenticated ? (
           <button className="navbar-button">Get Started</button>
         ) : (
-          <Link to="/sign-in">
-            <button
-              className="navbar-button-signin"
-              // onClick={authenticatedUser}
-            >
-              Sign In
-            </button>
-          </Link>
+          <button className="navbar-button-signin" onClick={authenticatedUser}>
+            Sign In
+          </button>
         )}
         {/* {isUserAuthenticated && (
             <button className="navbar-button">Get Started</button>
