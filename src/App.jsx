60c6202543bd03126a04f086c36d97aef3f86f57
Bhavesh Adhikari <bhaveshadhikari71@gmail.com>
2026-01-13 20:39:43 +0545

    team nested routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
 import "./styles/App.css";
 import Home from "./pages/Home";
 import Team from "./pages/Team";
+import TeamDetails from "./pages/TeamDetails";
 import Contact from "./pages/Contact";
 import Auth from "./pages/Auth";
 import { Routes, Route } from "react-router";
+
 
 function App() {
   return (
     <Routes>
       <Route index element={<Home />} />
       <Route path="/home" element={<Home />} />
-      <Route path="/team" element={<Team />} />
+      <Route path="/team">
+        <Route path =":teamId" element={<Team/>} />
+        <Route path ="details" element={<TeamDetails/>} />
+      </Route>
+      
       <Route path="/contact" element={<Contact />} />
       <Route path="/sign-in" element={<Auth />} />
+
+      <Route path="/error" element = {<h1>Internal Error</h1>}/>
+      <Route path="*" element = {<h1>404. Oppps!!</h1>}/>
+
     </Routes>
   );
 }
--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Navbar from "../component/NavBar";
 import AboutTeam from "../component/AboutTeam";
+import { useParams } from "react-router";
 
 function Team() {
+  // routing bata pathayeko teamId access gareko
+  const {teamId} = useParams();
+
   return (
     <div>
       <Navbar />
+      <h1>Current Team ID: {teamId} </h1>
       <AboutTeam />
     </div>
   );
