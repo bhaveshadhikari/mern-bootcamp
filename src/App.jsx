3611ff90bacfc8f6ecf539a15db8b3cc1a073c58
redmenIshab <redmenIshab>
2026-01-13 20:18:04 +0545

    adds nested routes and error handling

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,25 @@ import "./styles/App.css";
 import Home from "./pages/Home";
 import Team from "./pages/Team";
 import Auth from "./pages/Auth";
+import TeamDetails from "./pages/TeamDetails";
 import { Routes, Route } from "react-router";
 
 function App() {
   return (
     <Routes>
       <Route index element={<Home />} />
-      <Route path="/team" element={<Team />} />
+
       <Route path="/sign-in" element={<Auth />} />
+
+      {/* team nested route */}
+      <Route path="team">
+        <Route path=":teamId" element={<Team />} />
+        <Route path="details" element={<TeamDetails />} />
+      </Route>
+
+      {/* Default Route to handle error or not found page */}
+      <Route path="/error" element={<h1>500 Internal Server Error</h1>} />
+      <Route path="*" element={<h1>404 Not Found</h1>} />
     </Routes>
   );
 }
--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import Navbar from "../component/NavBar";
 import AboutTeam from "../component/AboutTeam";
+import { useParams } from "react-router";
 
 function Team() {
+  const { teamId } = useParams();
+  console.log("TEAM ID", teamId);
+  // id =3
+  // API call -> fetch information for team 3
   return (
     <div>
       <Navbar />
