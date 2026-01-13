27a4a1cb0034780e331fecad70ee84029d98f8a6
Bhavesh Adhikari <bhaveshadhikari71@gmail.com>
2026-01-13 18:52:22 +0545

    break contactpage  into component

---
+++ src/component/ContactForm.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { Link } from "react-router";
+function ContactForm() {
+
+    const [showForm, setShowForm] = useState(false);
+
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+
+    const handleSubmit = (e) => {
+
+        e.preventDefault()
+
+        console.log({
+            name,
+            email,
+            message,
+        });
+
+        alert("Enquiry submitted!");
+
+        // Clear form
+        setName("");
+        setEmail("");
+        setMessage("");
+    };
+
+    return (
+
+        <>
+            <Navbar />
+            <div style={{ width: "100%", display:"flex", justifyContent: "center", alignItems:"center" }}>
+
+
+                <div style={formStyle}>
+                    <h3>Enquiry Form</h3>
+
+                    <form onSubmit={handleSubmit}>
+                        <input type="text" style={formField} placeholder="Name" value={name} required onChange={(e) => { setName(e.target.value) }} /><br /><br />
+                        <input type="email" placeholder="Email" style={formField} value={email} required onChange={(e) => { setEmail(e.target.value) }} /><br /><br />
+                        <textarea placeholder="Message" style={formField} value={message} required onChange={(e) => { setMessage(e.target.value) }}></textarea><br /><br />
+
+                        <Button
+                            title="Submit"
+                            backgroundColor="#178748ff"
+                            type="submit"
+                        />
+                    </form>
+                </div>
+            </div>
+            <Footer />
+        </>
+    );
+}
+
+
+
+const formStyle = {
+    background: "#fff",
+    padding: 20,
+    borderRadius: 8,
+    width: 300,
+
+};
+
+const formField = {
+    fontSize: '18px',
+    width: '100%',
+    padding: '7px',
+
+};
+
+export default ContactForm;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,75 +2,11 @@ import React, { useState } from "react";
 import Button from "../component/Button.jsx";
 import Navbar from "../component/NavBar.jsx";
 import Footer from "../component/Footer.jsx";
-function ContactForm() {
+import ContactForm from "../component/ContactForm.jsx";
 
-    const [showForm, setShowForm] = useState(false);
-
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [message, setMessage] = useState("");
-
-    const handleSubmit = (e) => {
-
-        e.preventDefault()
-
-        console.log({
-            name,
-            email,
-            message,
-        });
-
-        alert("Enquiry submitted!");
-
-        // Clear form
-        setName("");
-        setEmail("");
-        setMessage("");
-    };
-
-    return (
-
-        <>
-            <Navbar />
-            <div style={{ width: "100%", display:"flex", justifyContent: "center", alignItems:"center" }}>
-
-
-                <div style={formStyle}>
-                    <h3>Enquiry Form</h3>
-
-                    <form onSubmit={handleSubmit}>
-                        <input type="text" style={formField} placeholder="Name" value={name} required onChange={(e) => { setName(e.target.value) }} /><br /><br />
-                        <input type="email" placeholder="Email" style={formField} value={email} required onChange={(e) => { setEmail(e.target.value) }} /><br /><br />
-                        <textarea placeholder="Message" style={formField} value={message} required onChange={(e) => { setMessage(e.target.value) }}></textarea><br /><br />
-
-                        <Button
-                            title="Submit"
-                            backgroundColor="#178748ff"
-                            type="submit"
-                        />
-                    </form>
-                </div>
-            </div>
-            <Footer />
-        </>
+function Contact(){
+    return(
+        <ContactForm/>
     );
 }
-
-
-
-const formStyle = {
-    background: "#fff",
-    padding: 20,
-    borderRadius: 8,
-    width: 300,
-
-};
-
-const formField = {
-    fontSize: '18px',
-    width: '100%',
-    padding: '7px',
-
-};
-
-export default ContactForm;
+export default Contact;
