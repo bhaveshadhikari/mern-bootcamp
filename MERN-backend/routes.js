cc090a292a24f7e9522b057adeee730305716a91
Bhavesh Adhikari <bhaveshadhikari71@gmail.com>
2026-01-26 20:21:12 +0545

    adds auth/login and token generation

--- MERN-backend/routes.js
+++ MERN-backend/routes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import handleAuth from './auth/auth.js'
+import loginController from './auth/loginController.js';
+
 const router = Router();
 
 
 // auth related routes
 router.get('/auth/', handleAuth)
-router.post('/auth/login', handleAuth)
+router.post('/auth/login', loginController)
 
 export default router;
